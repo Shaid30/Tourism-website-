fa1483c8f54edcbe4f22279382aab028a0f97e98
Abushaid <shaid@gmail.com>
2025-11-15 13:03:17 +0600

    add package

--- myapp/static/myapp/js/script.js
+++ myapp/static/myapp/js/script.js
@@ -17,4 +17,27 @@ setInterval(() => {
     hero.style.backgroundImage = `url('${images[i]}')`;
 }, 5000);
 
+// Read More Button
+// Read More Button
+document.querySelectorAll(".btn-more").forEach(btn => {
+    btn.addEventListener("click", function () {
+        const card = this.closest(".card");
+        const desc = card.querySelector(".desc");
+
+        desc.classList.toggle("expanded");
+
+        this.textContent = desc.classList.contains("expanded")
+            ? "Show Less"
+            : "Read More";
+    });
+});
+
+// Book Now Button
+document.querySelectorAll(".btn-book").forEach(btn => {
+    btn.addEventListener("click", function () {
+        const title = this.closest(".card").querySelector(".title").textContent;
+        alert("Booking started for: " + title);
+    });
+});
+
 
