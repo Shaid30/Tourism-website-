e2580dfc313bcc638737473d3bbf2ee173421deb
Abushaid <mdabushaid847@gmail.com>
2025-12-29 18:49:28 +0600

    final commit

---
+++ staticfiles/myapp/js/script.js
@@ -0,0 +1,36 @@
+const menuIcon = document.getElementById('menuIcon');
+const navLinks = document.getElementById('navLinks');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
+}
+
+
+const hero = document.querySelector(".hero");
+
+let i = 0;
+
+hero.style.backgroundImage = `url('${images[i]}')`;
+
+setInterval(() => {
+    i = (i + 1) % images.length;
+    hero.style.backgroundImage = `url('${images[i]}')`;
+}, 5000);
+
+document.querySelectorAll(".btn-more").forEach(btn => {
+    btn.addEventListener("click", function () {
+        const card = this.closest(".card");
+        const desc = card.querySelector(".desc");
+
+        desc.classList.toggle("expanded");
+
+        this.textContent = desc.classList.contains("expanded")
+            ? "Show Less"
+            : "Read More";
+    });
+});
+
+
+
+
+
