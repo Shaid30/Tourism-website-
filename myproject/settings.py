a5f13ca85abc81f02a3c0e6e6c0d2198db0354f0
Abushaid <mdabushaid847@gmail.com>
2025-12-29 19:38:10 +0600

    final commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_$aj=2vz=a57mzbkvz5275nnav9^=)-@e+wsv2n92e(wi-h5%k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ["myapp.onrender.com"]
 
 
 # Application definition
